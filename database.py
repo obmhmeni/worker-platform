68b77c412988652eea466ede5e0055f522390c11
obmhmeni <obmhmeni@gmail.com>
2026-03-08 06:18:28 +0530

    fixed pymongo issue

--- database.py
+++ database.py
@@ -1,11 +1,12 @@
 from pymongo import MongoClient
 
-MONGO_URL = "mongodb+srv://your_username:your_password@cluster.mongodb.net"
+# Temporary local connection
+MONGO_URL = "mongodb://localhost:27017"
 
 client = MongoClient(MONGO_URL)
 
 db = client["worker_platform"]
 
 users_collection = db["users"]
-jobs_collection = db["jobs"]
 workers_collection = db["workers"]
+jobs_collection = db["jobs"]
